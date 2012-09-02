8eb77a2fcd8caec28c20e8917b6212a1312af872
Feng Li <m@feng.li>
2012-09-02 16:17:01 +0200

    Add random phrase into the name string

--- stable/save.all.R
+++ stable/save.all.R
@@ -1,16 +1,16 @@
 ##' Save all the objects to a folder.
 ##'
-##' Details.
-##' @name 
-##' @title 
-##' @param path 
-##' @param ... 
-##' @return 
-##' @references 
+##' NA
+##' @name
+##' @title
+##' @param path
+##' @param ...
+##' @return NULL
 ##' @author Feng Li, Department of Statistics, Stockholm University, Sweden.
-##' @note First version: ; Current: .
+##' @note First version: Sun Sep 02 16:10:36 CEST 2012;
+##'       Current: Sun Sep 02 16:10:42 CEST 2012.
 save.all <- function(save.output, ModelDescription = NULL, Running.date)
-  { 
+  {
     if (save.output != FALSE) ## Save has been requested.
       {
         ## Creat the folder if not exits
@@ -18,14 +18,21 @@ save.all <- function(save.output, ModelDescription = NULL, Running.date)
           {
             dir.create(save.output)
           }
-        
-        OUT.file.name <- paste(file.path(save.output, paste(ModelDescription,
-                         format(Running.date, "%Y%b%d-%H.%M.%S"), sep = "_")), ".Rdata", sep = "") 
-        
+
+        ## Generate a random phrase to avoid file conflict
+        randomPhrase <- paste(as.character.hexmode(sample(101:999, 2)),
+                              sep = "", collapse="") ## simple hex character
+        OUT.file.name <- paste(
+            file.path(save.output,
+                      paste(ModelDescription,
+                            format(Running.date, "%Y%b%d-%H.%M.%S"),
+                            sep = "_")),
+            "_", randomPhrase, ".Rdata", sep = "")
+
         ## Save all the objects
         save.image(file = OUT.file.name)
-        
-        ## Save all the figures
+
+        ## Done message
         cat("Outputs saved in:\n",  paste("\"", OUT.file.name, "\"", sep = ""), "\n")
         cat("----------------------------------------------------------------------\n\n")
       }
