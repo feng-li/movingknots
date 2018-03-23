e55e3a6f962789df029494a7f880d0148b16969c
chengfan <chengfan9477@163.com>
2018-03-23 11:58:58 +0800

    error in calculating sigma to be solved

--- R/algorithms/SurfacePred.R
+++ R/algorithms/SurfacePred.R
@@ -1,11 +1,11 @@
-PredSurface <- function(Y, x0, OUT.params, jIter, iCross, splineArgs)
+PredSurface <- function(Y, x0, OUT.FITTED, jIter, iCross, splineArgs)
 {
     Params.j <- OUT.FITTED[["Params"]]
     # Params.j <- lapply(OUT.Params, function(x)
         # apply(x[, , jIter, iCross, drop = FALSE], c(1, 2), "["))
 
     ## diag.K <- Params.j[["shrinkages"]]
-    ## Sigma <- vech2m(Params.j[["covariance"]])
+    ## Sigma <- vech2m(Params.j[["covariance"]]) #???Error
     B <- Params.j[["coefficients"]][, , jIter, iCross]
     knots.mat <- Params.j[["knots"]]
     knots.list <- knots.mat2list(knots.mat[, , jIter, iCross], splineArgs)
@@ -20,6 +20,6 @@ PredSurface <- function(Y, x0, OUT.params, jIter, iCross, splineArgs)
 # ### Testing Rajan.Rdata
 # FITTED.file <- "~/running/tsfeature_s_moving_2_plus_a_moving_2+20180322@23.57.cb16cb.Rdata"
 # load(FITTED.file)
-# Y.pred <- PredSurface(Y, x0 = x, OUT.params, jIter = 1, iCross = 1, splineArgs)
+# Y.pred <- PredSurface(Y, x0 = x, OUT.FITTED, jIter = 1, iCross = 1, splineArgs)
 # head(Y.pred)
 # dim(Y.pred)
